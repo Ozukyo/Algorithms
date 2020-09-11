972a92ddbaa4a73efcaa4a40edb4d95f6848abd5
Ozukyo <ozukyo@gmail.com>
2020-09-11 14:42:00 +0200

    Add replacing dots with numbers

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -4,9 +4,9 @@ function generateRandomGrid() {
     var height = Math.floor(Math.random() * ((15 - 3 + 1) / 2) + 3) * 2;
     var mineOrDot = ".*";
     var gridLook = "";
-    for (var i = 0; i < width; i++) {
+    for (var i = 0; i < height; i++) {
         arr[i] = [];
-        for (var j = 0; j < height; j++) {
+        for (var j = 0; j < width; j++) {
             arr[i][j] = mineOrDot.charAt(Math.floor(Math.random() * mineOrDot.length));
             gridLook += arr[i][j] + " ";
         }
@@ -15,4 +15,36 @@ function generateRandomGrid() {
     console.log(gridLook);
     return arr;
 }
-console.log(generateRandomGrid());
+function fillGridWithNumbers(grid) {
+    var bombCounter = 0;
+    var gridLook = "";
+    for (var i = 0; i < grid.length - 1; i++) {
+        for (var j = 0; j < grid.length; j++) {
+            if (grid[i][j] === ".") {
+                if (grid[i + 1][j] !== undefined && grid[i + 1][j] === "*")
+                    bombCounter += 1;
+                if (grid[i + 1][j + 1] !== undefined && grid[i + 1][j + 1] === "*")
+                    bombCounter += 1;
+                if (grid[i][j + 1] !== undefined && grid[i][j + 1] === "*")
+                    bombCounter += 1;
+                if (grid[i - 1][j] !== undefined && grid[i - 1][j] === "*")
+                    bombCounter += 1;
+                if (grid[i - 1][j - 1] !== undefined && grid[i - 1][j - 1] === "*")
+                    bombCounter += 1;
+                if (grid[i][j - 1] !== undefined && grid[i][j - 1] === "*")
+                    bombCounter += 1;
+                if (grid[i - 1][j + 1] !== undefined && grid[i - 1][j + 1] === "*")
+                    bombCounter += 1;
+                if (grid[i + 1][j - 1] !== undefined && grid[i + 1][j - 1] === "*")
+                    bombCounter += 1;
+                grid[i][j] = bombCounter.toString();
+                bombCounter = 0;
+            }
+            gridLook += grid[i][j] + " ";
+        }
+        gridLook += "\n";
+    }
+    console.log(gridLook);
+    return grid;
+}
+fillGridWithNumbers(generateRandomGrid());
--- minesweeper/minesweeper.ts
+++ minesweeper/minesweeper.ts
@@ -1,13 +1,13 @@
 function generateRandomGrid(): string[][] {
     let arr: string[][] = [];
-    let width: number = Math.floor(Math.random() * ((20 - 3 + 1)/2) + 3)*2;
-    let height: number = Math.floor(Math.random() * ((15 - 3 + 1)/2) + 3)*2;
+    let width: number = Math.floor(Math.random() * ((20 - 3 + 1) / 2) + 3) * 2;
+    let height: number = Math.floor(Math.random() * ((15 - 3 + 1) / 2) + 3) * 2;
     let mineOrDot: string = ".*";
-    let gridLook: string = ""
+    let gridLook: string = "";
 
-    for (let i = 0; i < width; i++) {
+    for (let i = 0; i < height; i++) {
         arr[i] = [];
-        for (let j = 0; j < height; j++) {
+        for (let j = 0; j < width; j++) {
             arr[i][j] = mineOrDot.charAt(Math.floor(Math.random() * mineOrDot.length));
             gridLook += arr[i][j] + " ";
         }
@@ -17,4 +17,30 @@ function generateRandomGrid(): string[][] {
     return arr;
 }
 
-console.log(generateRandomGrid());
+function fillGridWithNumbers(grid: string[][]): any {
+    let bombCounter: number = 0;
+    let gridLook: string = "";
+
+    for (let i = 0; i < grid.length - 1; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            if (grid[i][j] === ".") {
+                if (grid[i + 1][j] !== undefined && grid[i + 1][j] === "*") bombCounter += 1;
+                if (grid[i + 1][j + 1] !== undefined && grid[i + 1][j + 1] === "*") bombCounter += 1;
+                if (grid[i][j + 1] !== undefined && grid[i][j + 1] === "*") bombCounter += 1;
+                if (grid[i - 1][j] !== undefined && grid[i - 1][j] === "*") bombCounter += 1;
+                if (grid[i - 1][j - 1] !== undefined && grid[i - 1][j - 1] === "*") bombCounter += 1;
+                if (grid[i][j - 1] !== undefined && grid[i][j - 1] === "*") bombCounter += 1;
+                if (grid[i - 1][j + 1] !== undefined && grid[i - 1][j + 1] === "*") bombCounter += 1;
+                if (grid[i + 1][j - 1] !== undefined && grid[i + 1][j - 1] === "*") bombCounter += 1;
+                grid[i][j] = bombCounter.toString();
+                bombCounter = 0;
+            }
+            gridLook += grid[i][j] + " ";
+        }
+        gridLook += "\n";
+    }
+    console.log(gridLook);
+    return grid;
+}
+
+fillGridWithNumbers(generateRandomGrid())
