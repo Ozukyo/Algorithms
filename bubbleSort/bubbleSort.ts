087cd7acfd6ca33a7e90e29a85ca0cf6637987ed
Ozukyo <ozukyo@gmail.com>
2020-09-12 14:37:14 +0200

    Optimize code execution

--- bubbleSort/bubbleSort.ts
+++ bubbleSort/bubbleSort.ts
@@ -1,12 +1,16 @@
 function bubbleSort(arr: number[]): number[] {
     let arrLength: number = arr.length;
-    for (let i = 0; i < arrLength; i++) {
-        for (let j = 0; j < arrLength; j++) {
+    let noSwaps: boolean;
+
+    for (let i = arrLength - 1; i < 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                noSwaps = false;
             }
         }
-        arrLength--;
+        if (noSwaps) break;
     }
     return arr;
 }
